4d2a728e9aac7dc61dbf3546d9cc4c0df8667e53
Dmitry Saychenko <sadmitry@gmail.com>
2023-05-25 12:13:44 +0300

    Update new_flash.sh

--- new_flash.sh
+++ new_flash.sh
@@ -2,6 +2,3 @@
 PROJECT=bluepill_midi_monster
 IMAGE=/home/dmitry/eclipse-workspace/$PROJECT/Debug/$PROJECT.hex
 echo -e "reset halt\r\nflash write_image erase $IMAGE\r\nreset\r\nexit\r\n" | nc localhost 4444
-
-#killall openocd
-
